bec47f408c06f43dc44d80b8589a5f13b882baf8
Vanessa <vanessa_ywlin@gmail.com>
2026-04-12 12:59:38 -0400

    1. PegDuotaire to use Bitwise PegBoard 2. Add MCTS and AlphaBeta Search (cutoff = 6) to Peg Duotaire 2 player game.

--- games4e.py
+++ games4e.py
@@ -13,7 +13,8 @@
 from collections import namedtuple, Counter, defaultdict
 import random
 import math
-import functools 
+import functools
+import numpy as np
 cache = functools.lru_cache(10**6)
 
 
@@ -67,69 +68,69 @@ def play_game(game, strategies: dict, verbose=False):
 # In[3]:
 
 
-def minimax_search(game, state):
-    """Search game tree to determine best move; return (value, move) pair."""
-
-    player = state.to_move
-
-    def max_value(state):
-        if game.is_terminal(state):
-            return game.utility(state, player), None
-        v, move = -infinity, None
-        for a in game.actions(state):
-            v2, _ = min_value(game.result(state, a))
-            if v2 > v:
-                v, move = v2, a
-        return v, move
-
-    def min_value(state):
-        if game.is_terminal(state):
-            return game.utility(state, player), None
-        v, move = +infinity, None
-        for a in game.actions(state):
-            v2, _ = max_value(game.result(state, a))
-            if v2 < v:
-                v, move = v2, a
-        return v, move
-
-    return max_value(state)
-
-infinity = math.inf
-
-def alphabeta_search(game, state):
-    """Search game to determine best action; use alpha-beta pruning.
-    As in [Figure 5.7], this version searches all the way to the leaves."""
-
-    player = state.to_move
-
-    def max_value(state, alpha, beta):
-        if game.is_terminal(state):
-            return game.utility(state, player), None
-        v, move = -infinity, None
-        for a in game.actions(state):
-            v2, _ = min_value(game.result(state, a), alpha, beta)
-            if v2 > v:
-                v, move = v2, a
-                alpha = max(alpha, v)
-            if v >= beta:
-                return v, move
-        return v, move
-
-    def min_value(state, alpha, beta):
-        if game.is_terminal(state):
-            return game.utility(state, player), None
-        v, move = +infinity, None
-        for a in game.actions(state):
-            v2, _ = max_value(game.result(state, a), alpha, beta)
-            if v2 < v:
-                v, move = v2, a
-                beta = min(beta, v)
-            if v <= alpha:
-                return v, move
-        return v, move
-
-    return max_value(state, -infinity, +infinity)
-
+# def minimax_search(game, state):
+#     """Search game tree to determine best move; return (value, move) pair."""
+#
+#     player = state.to_move
+#
+#     def max_value(state):
+#         if game.is_terminal(state):
+#             return game.utility(state, player), None
+#         v, move = -infinity, None
+#         for a in game.actions(state):
+#             v2, _ = min_value(game.result(state, a))
+#             if v2 > v:
+#                 v, move = v2, a
+#         return v, move
+#
+#     def min_value(state):
+#         if game.is_terminal(state):
+#             return game.utility(state, player), None
+#         v, move = +infinity, None
+#         for a in game.actions(state):
+#             v2, _ = max_value(game.result(state, a))
+#             if v2 < v:
+#                 v, move = v2, a
+#         return v, move
+#
+#     return max_value(state)
+#
+# infinity = math.inf
+#
+# def alphabeta_search(game, state):
+#     """Search game to determine best action; use alpha-beta pruning.
+#     As in [Figure 5.7], this version searches all the way to the leaves."""
+#
+#     player = state.to_move
+#
+#     def max_value(state, alpha, beta):
+#         if game.is_terminal(state):
+#             return game.utility(state, player), None
+#         v, move = -infinity, None
+#         for a in game.actions(state):
+#             v2, _ = min_value(game.result(state, a), alpha, beta)
+#             if v2 > v:
+#                 v, move = v2, a
+#                 alpha = max(alpha, v)
+#             if v >= beta:
+#                 return v, move
+#         return v, move
+#
+#     def min_value(state, alpha, beta):
+#         if game.is_terminal(state):
+#             return game.utility(state, player), None
+#         v, move = +infinity, None
+#         for a in game.actions(state):
+#             v2, _ = max_value(game.result(state, a), alpha, beta)
+#             if v2 < v:
+#                 v, move = v2, a
+#                 beta = min(beta, v)
+#             if v <= alpha:
+#                 return v, move
+#         return v, move
+#
+#     return max_value(state, -infinity, +infinity)
+#
 
 
 
@@ -283,34 +284,34 @@ def player(search_algorithm):
 # In[11]:
 
 
-def minimax_search_tt(game, state):
-    """Search game to determine best move; return (value, move) pair."""
-
-    player = state.to_move
-
-    @cache
-    def max_value(state):
-        if game.is_terminal(state):
-            return game.utility(state, player), None
-        v, move = -infinity, None
-        for a in game.actions(state):
-            v2, _ = min_value(game.result(state, a))
-            if v2 > v:
-                v, move = v2, a
-        return v, move
-
-    @cache
-    def min_value(state):
-        if game.is_terminal(state):
-            return game.utility(state, player), None
-        v, move = +infinity, None
-        for a in game.actions(state):
-            v2, _ = max_value(game.result(state, a))
-            if v2 < v:
-                v, move = v2, a
-        return v, move
-
-    return max_value(state)
+# def minimax_search_tt(game, state):
+#     """Search game to determine best move; return (value, move) pair."""
+#
+#     player = state.to_move
+#
+#     @cache
+#     def max_value(state):
+#         if game.is_terminal(state):
+#             return game.utility(state, player), None
+#         v, move = -infinity, None
+#         for a in game.actions(state):
+#             v2, _ = min_value(game.result(state, a))
+#             if v2 > v:
+#                 v, move = v2, a
+#         return v, move
+#
+#     @cache
+#     def min_value(state):
+#         if game.is_terminal(state):
+#             return game.utility(state, player), None
+#         v, move = +infinity, None
+#         for a in game.actions(state):
+#             v2, _ = max_value(game.result(state, a))
+#             if v2 < v:
+#                 v, move = v2, a
+#         return v, move
+#
+#     return max_value(state)
 
 
 # For alpha-beta search, we can still use a cache, but it should be based just on the state, not on whatever values alpha and beta have.
@@ -327,41 +328,41 @@ def cache1(function):
         return cache[x]
     return wrapped
 
-def alphabeta_search_tt(game, state):
-    """Search game to determine best action; use alpha-beta pruning.
-    As in [Figure 5.7], this version searches all the way to the leaves."""
-
-    player = state.to_move
-
-    @cache1
-    def max_value(state, alpha, beta):
-        if game.is_terminal(state):
-            return game.utility(state, player), None
-        v, move = -infinity, None
-        for a in game.actions(state):
-            v2, _ = min_value(game.result(state, a), alpha, beta)
-            if v2 > v:
-                v, move = v2, a
-                alpha = max(alpha, v)
-            if v >= beta:
-                return v, move
-        return v, move
-
-    @cache1
-    def min_value(state, alpha, beta):
-        if game.is_terminal(state):
-            return game.utility(state, player), None
-        v, move = +infinity, None
-        for a in game.actions(state):
-            v2, _ = max_value(game.result(state, a), alpha, beta)
-            if v2 < v:
-                v, move = v2, a
-                beta = min(beta, v)
-            if v <= alpha:
-                return v, move
-        return v, move
-
-    return max_value(state, -infinity, +infinity)
+# def alphabeta_search_tt(game, state):
+#     """Search game to determine best action; use alpha-beta pruning.
+#     As in [Figure 5.7], this version searches all the way to the leaves."""
+#
+#     player = state.to_move
+#
+#     @cache1
+#     def max_value(state, alpha, beta):
+#         if game.is_terminal(state):
+#             return game.utility(state, player), None
+#         v, move = -infinity, None
+#         for a in game.actions(state):
+#             v2, _ = min_value(game.result(state, a), alpha, beta)
+#             if v2 > v:
+#                 v, move = v2, a
+#                 alpha = max(alpha, v)
+#             if v >= beta:
+#                 return v, move
+#         return v, move
+#
+#     @cache1
+#     def min_value(state, alpha, beta):
+#         if game.is_terminal(state):
+#             return game.utility(state, player), None
+#         v, move = +infinity, None
+#         for a in game.actions(state):
+#             v2, _ = max_value(game.result(state, a), alpha, beta)
+#             if v2 < v:
+#                 v, move = v2, a
+#                 beta = min(beta, v)
+#             if v <= alpha:
+#                 return v, move
+#         return v, move
+#
+#     return max_value(state, -infinity, +infinity)
 
 
 # In[13]:
@@ -470,3 +471,67 @@ def report(game, searchers):
 
 # report(TicTacToe(), (alphabeta_search_tt,  alphabeta_search, h_alphabeta_search, minimax_search_tt))
 
+# ______________________________________________________________________________
+# Monte Carlo tree node and ucb function
+
+
+class MCT_Node:
+    """Node in the Monte Carlo search tree, keeps track of the children states."""
+
+    def __init__(self, parent=None, state=None, U=0, N=0):
+        self.__dict__.update(parent=parent, state=state, U=U, N=N)
+        self.children = {}
+        self.actions = None
+
+
+def ucb(n, C=1.4):
+    return np.inf if n.N == 0 else n.U / n.N + C * np.sqrt(np.log(n.parent.N) / n.N)
+
+# def monte_carlo_tree_search(state, game, N=1000):
+def monte_carlo_tree_search(game, state, N=1000):
+
+    def select(n):
+        """select a leaf node in the tree"""
+        if n.children:
+            return select(max(n.children.keys(), key=ucb))
+        else:
+            return n
+
+    def expand(n):
+        """expand the leaf node by adding all its children states"""
+        if not n.children and not game.terminal_test(n.state):
+            n.children = {MCT_Node(state=game.result(n.state, action), parent=n): action
+                          for action in game.actions(n.state)}
+        return select(n)
+
+    def simulate(game, state):
+        """simulate the utility of current state by random picking a step"""
+        player = game.to_move(state)
+        while not game.terminal_test(state):
+            action = random.choice(list(game.actions(state)))
+            state = game.result(state, action)
+        v = game.utility(state, player)
+        return -v
+
+    def backprop(n, utility):
+        """passing the utility back to all parent nodes"""
+        if utility > 0:
+            n.U += utility
+        # if utility == 0:
+        #     n.U += 0.5
+        n.N += 1
+        if n.parent:
+            backprop(n.parent, -utility)
+
+    root = MCT_Node(state=state)
+
+    for _ in range(N):
+        leaf = select(root)
+        child = expand(leaf)
+        result = simulate(game, child.state)
+        backprop(child, result)
+
+    max_state = max(root.children, key=lambda p: p.N)
+
+    # return root.children.get(max_state)
+    return (0, root.children.get(max_state))
--- peg_duotaire.py
+++ peg_duotaire.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python3
-from games4e import Game
+from games4e import *
 from peg_board import *
 
 class PegDuotaire(Game):
     """PegDuotaire, a subclass of Game, represents one player in a Peg Duotaire game."""
     def __init__(self, shape='English'):
         assert shape == 'English' or shape == 'Triangle'
+        self.shape = shape
         if shape == 'English':
-            board = EnglishPegBoardDict(to_move='X')
+            # board = EnglishPegBoardDict(to_move='X')
+            board = EnglishPegBoardInt(to_move='X')
+        elif shape == 'French':
+            board = FrenchPegBoardInt(to_move='X')
         elif shape == 'Triangle':
-            board = TrianglePegBoardDict(to_move='X')
+            # board = TrianglePegBoardDict(to_move='X')
+            board = TrianglePegBoardInt(to_move='X')
 
         self.initial = board
+        if isinstance(board, PegBoardDict):
+            self.goal = board.init_hole
+        else:
+            self.goal = copy.copy(self.initial)
+            self.goal.state = 1 << board.GOAL_INDEX
+            self.goal.pagoda = self.goal.compute_pagoda(self.goal.state)
+            self.goal.__dict__.pop("_canon", None)
+            self.goal.__dict__.pop("_hash", None)
 
     def actions(self, board):
         """Return a collection of the allowable moves from this state."""
@@ -25,7 +38,13 @@ class PegDuotaire(Game):
 
     def is_terminal(self, board):
         """Return True if this is a final state for the game."""
-        return not board.actions()
+        return len(board.actions()) == 0
+
+    def terminal_test(self, board):
+        return self.is_terminal(board)
+
+    def to_move(self, board):
+        return board.to_move
 
     def utility(self, board, player):
         """Return the value of this final state to player."""
